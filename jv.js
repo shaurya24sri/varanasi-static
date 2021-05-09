777da6dd45646f5489ce8d70bd33957600625005
shaurya24sri <81847195+shaurya24sri@users.noreply.github.com>
2021-05-09 13:33:45 +0530

    Add files via upload

---
+++ jv.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toodle');
+const showcase = document.querySelector('.head');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toodle('active');
+  head.classList.toodle('active');
+})
